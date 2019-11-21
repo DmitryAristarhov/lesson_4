d1ea132e9ef2dcbd908c8cc02d902c2cff7a6639
DmitryAristarhov <aristarhov@gmail.com>
2019-11-21 16:22:54 +0500

    Второе задание

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ print(F(3, 'aaaaaaa', 'bbbbbbbbb', 'ccccccccccc', 'dddddddd', 'eee', 'fffffffff'
 lst = ['0000000', '11111111', '22222222222', '3333333', '44444444', '555555555', '666666666666', '777777777', '88888888888',
        '9999', '000', '111111', '222222', '3333333', '444', '555555555', '666666', '77777777', '88888', '9999999999']
 print(len(lst), F(100, *lst))
+
+print()
+###############################################################################
+# 2. Напишите функцию вывода самого частого имени из списка на выходе функции F.
+
+from collections import Counter
+def max_count(lst):
+    count = Counter(lst)
+    return max(count, key = lambda x: count[x])
+
+l = F(100, *lst)
+print(l)
+print(Counter(l))
+print(max_count(l))
